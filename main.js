7337557d223f3f4750de3754c67b853c8cbfd4b1
bun2023 <138096357+bun2023@users.noreply.github.com>
2023-07-22 12:53:08 +0900

    portfolio
    Webデザイン練習

---
+++ main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+{
+
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    speed: 4000,
+    effect: 'fade',
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+  }) ;
+}
